e32bd805117b9125b55b1d213c434cd51911a0f7
Daniel <daniel.marcel.heer@gmail.com>
2022-03-05 22:03:55 +0100

    fix visual studio messed code up

--- src/components/Entity.vue
+++ src/components/Entity.vue
@@ -1,11 +1,4 @@
 <template>
-  <!--
-  TODO
-    - clickable Actions (attack,parade)
-    - effects (overHeal)
-    - duplicate entity
-    - save & load with local storage => trigger in parent also triggers entities function
-  -->
   <div class="entity" :class="{'active':isActive}">
     <div v-if="isEditMode" >
       <div class="editmode-controls">
@@ -13,7 +6,7 @@
           <svg width="13px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><title>lock</title><path d="M80 192V144C80 64.47 144.5 0 224 0C303.5 0 368 64.47 368 144V192H384C419.3 192 448 220.7 448 256V448C448 483.3 419.3 512 384 512H64C28.65 512 0 483.3 0 448V256C0 220.7 28.65 192 64 192H80zM144 192H304V144C304 99.82 268.2 64 224 64C179.8 64 144 99.82 144 144V192z"/></svg>
         </a>
         <a @click="$emit('remove', id)">
-                <svg width="13px" xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 105.7 122.88"><title>remove</title><path d="M30.46,14.57V5.22A5.18,5.18,0,0,1,32,1.55v0A5.19,5.19,0,0,1,35.68,0H70a5.22,5.22,0,0,1,3.67,1.53l0,0a5.22,5.22,0,0,1,1.53,3.67v9.35h27.08a3.36,3.36,0,0,1,3.38,3.37V29.58A3.38,3.38,0,0,1,102.32,33H98.51l-8.3,87.22a3,3,0,0,1-2.95,2.69H18.43a3,3,0,0,1-3-2.95L7.19,33H3.37A3.38,3.38,0,0,1,0,29.58V17.94a3.36,3.36,0,0,1,3.37-3.37Zm36.27,0V8.51H39v6.06ZM49.48,49.25a3.4,3.4,0,0,1,6.8,0v51.81a3.4,3.4,0,1,1-6.8,0V49.25ZM69.59,49a3.4,3.4,0,1,1,6.78.42L73,101.27a3.4,3.4,0,0,1-6.78-.43L69.59,49Zm-40.26.42A3.39,3.39,0,1,1,36.1,49l3.41,51.8a3.39,3.39,0,1,1-6.77.43L29.33,49.46ZM92.51,33.38H13.19l7.94,83.55H84.56l8-83.55Z"/></svg>
+              <svg width="13px" xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 105.7 122.88"><title>remove</title><path d="M30.46,14.57V5.22A5.18,5.18,0,0,1,32,1.55v0A5.19,5.19,0,0,1,35.68,0H70a5.22,5.22,0,0,1,3.67,1.53l0,0a5.22,5.22,0,0,1,1.53,3.67v9.35h27.08a3.36,3.36,0,0,1,3.38,3.37V29.58A3.38,3.38,0,0,1,102.32,33H98.51l-8.3,87.22a3,3,0,0,1-2.95,2.69H18.43a3,3,0,0,1-3-2.95L7.19,33H3.37A3.38,3.38,0,0,1,0,29.58V17.94a3.36,3.36,0,0,1,3.37-3.37Zm36.27,0V8.51H39v6.06ZM49.48,49.25a3.4,3.4,0,0,1,6.8,0v51.81a3.4,3.4,0,1,1-6.8,0V49.25ZM69.59,49a3.4,3.4,0,1,1,6.78.42L73,101.27a3.4,3.4,0,0,1-6.78-.43L69.59,49Zm-40.26.42A3.39,3.39,0,1,1,36.1,49l3.41,51.8a3.39,3.39,0,1,1-6.77.43L29.33,49.46ZM92.51,33.38H13.19l7.94,83.55H84.56l8-83.55Z"/></svg>
         </a>
       </div>
       <div class="editmode">
@@ -224,3 +217,18 @@ a{
   justify-content: flex-end;
   gap: 0.3em;
 }
+.editmode{
+  display: flex;
+  flex-direction: column;
+  align-content: space-between;
+  align-items: flex-end;
+  flex-wrap: wrap;
+  gap: 0.3em;
+}
+.editmode > div > label{
+  margin-right: 0.5em;
+}
+.editmode > div > input{
+  width: 7em;
+}
+</style>
